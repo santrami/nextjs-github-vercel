7f7456114e35a2e29aaeb9de470d32c20229571e
Santiago Ramírez <santiago.ramirez@bsc.es>
2024-02-06 15:10:22 +0100

    saving tailwind changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,72 @@
-import React from 'react'
+"use client"
+import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage, Form } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import React from "react";
+import { useForm } from "react-hook-form";
+
 
 function page() {
+  const form = useForm()
   return (
-    <div className='flex w-1/3 p-8 justify-center items-center'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat possimus deleniti accusamus ex sint expedita nihil exercitationem eveniet minima, ullam necessitatibus commodi nisi error delectus optio quia, voluptas dolore assumenda.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat possimus deleniti accusamus ex sint expedita nihil exercitationem eveniet minima, ullam necessitatibus commodi nisi error delectus optio quia, voluptas dolore assumenda.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat possimus deleniti accusamus ex sint expedita nihil exercitationem eveniet minima, ullam necessitatibus commodi nisi error delectus optio quia, voluptas dolore assumenda.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat possimus deleniti accusamus ex sint expedita nihil exercitationem eveniet minima, ullam necessitatibus commodi nisi error delectus optio quia, voluptas dolore assumenda.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat possimus deleniti accusamus ex sint expedita nihil exercitationem eveniet minima, ullam necessitatibus commodi nisi error delectus optio quia, voluptas dolore assumenda.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat possimus deleniti accusamus ex sint expedita nihil exercitationem eveniet minima, ullam necessitatibus commodi nisi error delectus optio quia, voluptas dolore assumenda.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat possimus deleniti accusamus ex sint expedita nihil exercitationem eveniet minima, ullam necessitatibus commodi nisi error delectus optio quia, voluptas dolore assumenda.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat possimus deleniti accusamus ex sint expedita nihil exercitationem eveniet minima, ullam necessitatibus commodi nisi error delectus optio quia, voluptas dolore assumenda.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat possimus deleniti accusamus ex sint expedita nihil exercitationem eveniet minima, ullam necessitatibus commodi nisi error delectus optio quia, voluptas dolore assumenda.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat possimus deleniti accusamus ex sint expedita nihil exercitationem eveniet minima, ullam necessitatibus commodi nisi error delectus optio quia, voluptas dolore assumenda.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat possimus deleniti accusamus ex sint expedita nihil exercitationem eveniet minima, ullam necessitatibus commodi nisi error delectus optio quia, voluptas dolore assumenda.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat possimus deleniti accusamus ex sint expedita nihil exercitationem eveniet minima, ullam necessitatibus commodi nisi error delectus optio quia, voluptas dolore assumenda.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat possimus deleniti accusamus ex sint expedita nihil exercitationem eveniet minima, ullam necessitatibus commodi nisi error delectus optio quia, voluptas dolore assumenda.</div>
-  )
+    <div className="container">
+      <div className="flex h-screen justify-center pt-8">
+        <div className="text-center pr-8">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat
+          possimus deleniti accusamus ex sint expedita nihil exercitationem
+          eveniet minima, ullam necessitatibus commodi nisi error delectus optio
+          quia, voluptas dolore assumenda.Lorem ipsum dolor, sit amet
+          consectetur adipisicing elit. Repellat possimus deleniti accusamus ex
+          sint expedita nihil exercitationem eveniet minima, ullam
+          necessitatibus commodi nisi error delectus optio quia, voluptas dolore
+          assumenda.Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Repellat possimus deleniti accusamus ex sint expedita nihil
+          exercitationem eveniet minima, ullam necessitatibus commodi nisi error
+          delectus optio quia, voluptas dolore assumenda.Lorem ipsum dolor, sit
+          amet consectetur adipisicing elit. Repellat possimus deleniti
+          accusamus ex sint expedita nihil exercitationem eveniet minima, ullam
+          necessitatibus commodi nisi error delectus optio quia, voluptas dolore
+          assumenda.Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Repellat possimus deleniti accusamus ex sint expedita nihil
+          exercitationem eveniet minima, ullam necessitatibus commodi nisi error
+          delectus optio quia, voluptas dolore assumenda.Lorem ipsum dolor, sit
+          amet consectetur adipisicing elit. Repellat possimus deleniti
+          accusamus ex sint expedita nihil exercitationem eveniet minima, ullam
+          necessitatibus commodi nisi error delectus optio quia, voluptas dolore
+          assumenda.Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Repellat possimus deleniti accusamus ex sint expedita nihil
+          exercitationem eveniet minima, ullam necessitatibus commodi nisi error
+          delectus optio quia, voluptas dolore assumenda.Lorem ipsum dolor, sit
+          amet consectetur adipisicing elit. Repellat possimus deleniti
+          accusamus ex sint expedita nihil exercitationem eveniet minima, ullam
+          necessitatibus commodi nisi error delectus optio quia, voluptas dolore
+          assumenda.Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Repellat possimus deleniti accusamus ex sint expedita nihil
+          exercitationem eveniet minima, ullam necessitatibus commodi nisi error
+          delectus optio quia, voluptas dolore assumenda.Lorem ipsum dolor, sit
+          amet consectetur adipisicing elit. Repellat possimus deleniti
+          accusamus ex sint expedita nihil exercitationem eveniet minima, ullam
+          necessitatibus commodi nisi error delectus optio quia, voluptas dolore
+          assumenda.Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Repellat possimus deleniti accusamus ex sint expedita nihil
+          exercitationem eveniet minima, ullam necessitatibus commodi nisi error
+          delectus optio quia, voluptas dolore assumenda.Lorem ipsum dolor, sit
+          amet consectetur adipisicing elit. Repellat possimus deleniti
+          accusamus ex sint expedita nihil exercitationem eveniet minima, ullam
+          necessitatibus commodi nisi error delectus optio quia, voluptas dolore
+          assumenda.Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Repellat possimus deleniti accusamus ex sint expedita nihil
+          exercitationem eveniet minima, ullam necessitatibus commodi nisi error
+          delectus optio quia, voluptas dolore assumenda.
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+          beatae, quisquam facilis est provident officia voluptatum, excepturi
+          officiis a quaerat quis ad eos reiciendis id? Voluptates ratione nisi
+          esse libero.
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default page
+export default page;
